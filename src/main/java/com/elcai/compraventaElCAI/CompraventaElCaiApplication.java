70ce04a5692f549154457f6fa8aa4f2da203e51c
Manuela <manuelamunozlujan0@gmail.com>
2025-05-05 09:51:21 -0500

    Iniciando repo

---
+++ src/main/java/com/elcai/compraventaElCAI/CompraventaElCaiApplication.java
@@ -0,0 +1,13 @@
+package com.elcai.compraventaElCAI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompraventaElCaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompraventaElCaiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elcai/compraventaElCAI/CompraventaElCaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elcai.compraventaElCAI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompraventaElCaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
